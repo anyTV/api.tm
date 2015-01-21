29ba36cd6388f77eb6b9d5ea3de5b56be3b6686e
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-01-21 11:20:16 +0800

    Replaced connect-busboy  with multer

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ logger.log('verbose', 'Binding 3rd-party middlewares');
 app.use(require('morgan')('combined', {stream : util.get_log_stream(config.LOGS_DIR)}));
 app.use(require('method-override')());
 app.use(body_parser.urlencoded({extended : false}));
-app.use(require('connect-busboy')());
+app.use(require('multer')({dest : config.UPLOAD_DIR}));
 app.use(require('compression')());
 
 logger.log('verbose', 'Binding custom middlewares');
