3ae6c948b2b4043d22295e91e073aea6757a0157
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-08-06 09:49:55 +0800

    Fixed html file rendering

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ logger.log('info', 'Starting', config.APP_NAME, 'on', config.ENV, 'environment')
 
 app.disable('x-powered-by');
 
-app.set('views', config.VIEWS_DIR);
-app.set('view engine', 'jade');
 
 logger.log('verbose', 'Binding 3rd-party middlewares');
 
 app.use(require('morgan')('combined', {stream: util.get_log_stream(config.LOGS_DIR)}));
+app.set(express.static(config.VIEWS_DIR));
 app.use(require('method-override')());
 app.use(body_parser.urlencoded({extended: true}));
 app.use(body_parser.json());
