ff109a8b15f5af001cca66c87f7752696429e1bc
ravenjohn <ravenjohn@users.noreply.github.com>
2015-10-12 23:21:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.disable('x-powered-by');
 logger.log('verbose', 'Binding 3rd-party middlewares');
 
 app.use(require('morgan')('combined', {stream: util.get_log_stream(config.LOGS_DIR)}));
-app.set(express.static(config.VIEWS_DIR));
+app.use(express.static(config.VIEWS_DIR));
 app.use(require('method-override')());
 app.use(body_parser.urlencoded({extended: true}));
 app.use(body_parser.json());
