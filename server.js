c6306dd253ee55c0fd0755ad6f8dca0e953b8b42
ravenjohn <ravenjohn@users.noreply.github.com>
2015-10-30 15:39:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ logger.log('verbose', 'Binding 3rd-party middlewares');
 app.use(require('morgan')('combined', {stream: util.get_log_stream(config.LOGS_DIR)}));
 app.use(express.static(config.VIEWS_DIR));
 app.use(require('method-override')());
-app.use(body_parser.urlencoded({extended: true}));
+app.use(body_parser.urlencoded({extended: false}));
 app.use(body_parser.json());
 app.use(require('compression')());
 
