071d9d90599511e9d385cc16f321ba4dbf6e4607
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-11-16 16:10:26 +0800

    Fixed indention

--- server.js
+++ server.js
@@ -15,57 +15,57 @@ let app;
 let handler;
 
 function start () {
-	if (handler) {
-		handler.close();
-	}
+    if (handler) {
+        handler.close();
+    }
 
-	// create express app
-	app = express();
+    // create express app
+    app = express();
 
-	// set config
-	config.use(process.env.NODE_ENV);
-	app.raven = config.ENV;
-	app.set('env', config.ENV);
+    // set config
+    config.use(process.env.NODE_ENV);
+    app.raven = config.ENV;
+    app.set('env', config.ENV);
 
-	// configure logger
-	winston.cli();
-	winston.level = config.LOG_LEVEL || 'silly';
+    // configure logger
+    winston.cli();
+    winston.level = config.LOG_LEVEL || 'silly';
 
-	// configure mysql
-	mysql.set_logger(winston)
-		.add('my_db', config.DB);
-
-
-	winston.log('info', 'Starting', config.APP_NAME, 'on', config.ENV, 'environment');
-
-	// configure express app
-	app.set('case sensitive routing', true);
-	app.set('x-powered-by', false);
-
-	winston.log('verbose', 'Binding 3rd-party middlewares');
-	app.use(require('morgan')('combined', {stream: util.get_log_stream(config.LOGS_DIR)}));
-	app.use(express.static(config.ASSETS_DIR));
-	app.use(require('method-override')());
-	app.use(body_parser.urlencoded({extended: false}));
-	app.use(body_parser.json());
-	app.use(require('compression')());
+    // configure mysql
+    mysql.set_logger(winston)
+        .add('my_db', config.DB);
 
 
-	winston.log('verbose', 'Binding custom middlewares');
-	app.use(require('anytv-node-cors')(config.CORS));
-	app.use(require(__dirname + '/lib/res_extended')());
-	app.use(require(__dirname + '/config/router')(express.Router()));
-	app.use(require('anytv-node-error-handler')(winston));
+    winston.log('info', 'Starting', config.APP_NAME, 'on', config.ENV, 'environment');
 
-	winston.log('info', 'Server listening on port', config.PORT)
+    // configure express app
+    app.set('case sensitive routing', true);
+    app.set('x-powered-by', false);
 
-	return app.listen(config.PORT);
+    winston.log('verbose', 'Binding 3rd-party middlewares');
+    app.use(require('morgan')('combined', {stream: util.get_log_stream(config.LOGS_DIR)}));
+    app.use(express.static(config.ASSETS_DIR));
+    app.use(require('method-override')());
+    app.use(body_parser.urlencoded({extended: false}));
+    app.use(body_parser.json());
+    app.use(require('compression')());
+
+
+    winston.log('verbose', 'Binding custom middlewares');
+    app.use(require('anytv-node-cors')(config.CORS));
+    app.use(require(__dirname + '/lib/res_extended')());
+    app.use(require(__dirname + '/config/router')(express.Router()));
+    app.use(require('anytv-node-error-handler')(winston));
+
+    winston.log('info', 'Server listening on port', config.PORT)
+
+    return app.listen(config.PORT);
 }
 
 handler = start();
 
 module.exports = {
-	app,
-	start,
-	handler
+    app,
+    start,
+    handler
 };
